cc92c903a8aedb14e5fb4e68c64be8d00dccc27e
lei li <oscarlee0202@gmail.com>
2020-03-01 15:24:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bigmall-version 2020-02-29
+
+mall-user 端口号8000
---
+++ mall-user/src/main/java/com/kq/mall/MallUserApplication.java
@@ -0,0 +1,15 @@
+package com.kq.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lilei
+ */
+@SpringBootApplication
+
+public class MallUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallUserApplication.class,args);
+    }
+}
